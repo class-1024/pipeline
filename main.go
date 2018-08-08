67c574c5197e520c5fa52cc3f5129e73f80eacba
gudaoling <1965948351@qq.com>
2018-08-08 11:51:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,10 +5,26 @@ import (
 )
 
 func main() {
+
 	router := gin.Default()
 	router.GET("/index", GetHome)
+	mqtt := router.Group("/mqtt/api/")
+	{
+		mqtt.POST("info", GetMqtt)
+	}
+	iot := router.Group("/iot/api/")
+	{
+		iot.POST("info", GetIot)
+	}
 	router.Run(":80")
 }
+func GetMqtt(c *gin.Context) {
+	c.JSON(200, gin.H{"code": 0, "message": c.DefaultQuery("name", "wehcome to mqtt api")})
+}
+func GetIot(c *gin.Context) {
+	c.JSON(200, gin.H{"code": 0, "message": c.DefaultQuery("name", "wehcome to iot api")})
+}
+
 func GetHome(c *gin.Context) {
-	c.JSON(200, gin.H{"code": 0, "message": c.DefaultQuery("name", "welcome to kinot")})
+	c.JSON(200, gin.H{"code": 0, "message": c.DefaultQuery("name", "welcome to kinot index")})
 }
